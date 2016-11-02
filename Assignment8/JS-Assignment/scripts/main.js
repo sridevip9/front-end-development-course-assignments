704ae0b03ee39fbd968fd5ae05d0fe953009d8f0
sridevi peddi <sridevip9@yahoo.com>
2016-11-01 21:01:20 -0500

    Assignment 8

---
+++ Assignment8/JS-Assignment/scripts/main.js
@@ -0,0 +1,8 @@
+var currentDayOfWeek='Tuesday';
+console.log(' Today is ',currentDayOfWeek);
+
+var numberOfCupsOfCoffee = 5;
+console.log('Number of times i had coffee today: '+numberOfCupsOfCoffee);
+
+var shirtColor = 'green';
+console.log('I am wearing shirt color is '+shirtColor);
