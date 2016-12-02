c6b9cbcf9e0bb5644f9ca7b68cd68c0292f3d105
sridevi peddi <sridevip9@yahoo.com>
2016-12-01 19:01:47 -0600

    Assignment 16 jQuery changes

--- Assignment16/scripts/main.js
+++ Assignment16/scripts/main.js
@@ -1,5 +1,50 @@
 $(document).ready(function(){
+//part-2
+$('#btnShow').on('click',function(){
+$('.display-basic').show()
+});
 
 
+$('#btnHide').on('click', function() {
+    $('.display-basic').hide();
+})
+
+$('#btnToggle').on('click', function() {
+    $('.display-basic').toggle();
+})
+//part-3
+
+$('.part3').show()
+
+
+
+ //part-4
+
+$('#btnFadeIn').on('click', function() {
+    $('.display-fade').fadeIn('slow');
+});
+
+
+$('#btnFadeOut').on('click', function() {
+    $('.display-fade').fadeOut('fast');
+})
+
+
+$('#btnFadeToggle').on('click', function() {
+    $('.display-fade').fadeToggle(5000);
+})
+// part-5
+
+$('#btnSlideDown').on('click',function(){
+    $('.display-slide').slideDown(1000);
+})
+
+$('#btnSlideUp').on('click',function(){
+    $('.display-slide').slideUp(500);
+})
+
+$('#btnSlideToggle').on('click',function(){
+    $('.display-slide').slideToggle();
+})
 
 });
