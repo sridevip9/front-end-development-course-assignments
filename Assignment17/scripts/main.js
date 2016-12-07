ed15889d595a97631cdc63e889fc87640f065c25
sridevi peddi <sridevip9@yahoo.com>
2016-12-06 19:26:14 -0600

    More jQuery

--- Assignment17/scripts/main.js
+++ Assignment17/scripts/main.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
 
+$('.error').hide();
 
+ $('.input-group').filter(":even").addClass('even');
 
+$('#firstName').on('blur', function(){
+	var input = $(this);
+  
+	if(input.val() === ''){
+  	$(this).closest('div').find('span').fadeIn('error requiredMsg');
+  }else{
+  		$(this).closest('div').find('span').fadeOut('error requiredMsg');
+  }
+})
+$('#lastName').on('blur', function(){
+	var input1 = $(this);
+  
+	if(input1.val() === ''){
+  	$(this).closest('div').find('span').fadeIn('error requiredMsg');
+  }else{
+  		$(this).closest('div').find('span').fadeOut('error requiredMsg');
+  }
+})
+
+$('#emailAddress').on('blur', function(){
+	var input2 = $(this);
+  
+	if(input2.val().indexOf('@')||input2.indexOf('.com')){
+  	$(this).closest('div').find('span').fadeIn('error emailMsg');
+  }else{
+  		$(this).closest('div').find('span').fadeOut('error emailMsg');
+  }
+})
 });
