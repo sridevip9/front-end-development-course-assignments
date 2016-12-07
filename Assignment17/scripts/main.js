359364b4a13cabd4125360df0683311e3820f475
sridevi peddi <sridevip9@yahoo.com>
2016-12-06 19:36:00 -0600

    email validation changes

--- Assignment17/scripts/main.js
+++ Assignment17/scripts/main.js
@@ -26,7 +26,7 @@ $('#lastName').on('blur', function(){
 $('#emailAddress').on('blur', function(){
 	var input2 = $(this);
   
-	if(input2.val().indexOf('@')||input2.indexOf('.com')){
+	if(input2.val().indexOf('@') === -1 || input2.val().indexOf('.com')=== -1){
   	$(this).closest('div').find('span').fadeIn('error emailMsg');
   }else{
   		$(this).closest('div').find('span').fadeOut('error emailMsg');
