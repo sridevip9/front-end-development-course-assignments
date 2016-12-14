dc38f97b4b673763b370215feb5310b6089abe4d
sridevi peddi <sridevip9@yahoo.com>
2016-12-13 18:59:07 -0600

    Assignment 19 json

--- Assignment19/scripts/main.js
+++ Assignment19/scripts/main.js
@@ -2,10 +2,25 @@ $(document).ready(function () {
 
     //List Titles
     var appendUrl = 'http://jsonplaceholder.typicode.com/posts?userId=1';
-
-
+    
+    $.get(appendUrl,function(result){
+        console.log(result);
+        var appendnew = $('#append');
+        for(var i = 0; i < result.length; i++){
+          appendnew.append('<li>' + 'Index:'+ i + 'Title:'+ result[i].title +'</li>'); 
+        }
+    });
     //
     var prependUrl = 'http://jsonplaceholder.typicode.com/photos?albumId=1';
-
-
-});
+     
+    $.get(prependUrl,function(data){
+        var prependnew = $('#prepend');
+        for(var i = 0; i < data.length; i++){
+          prependnew.prepend('<li>' + 'Index:'+ i + 'Title:'+ data[i].title +'</li>'); 
+        }
+        });
+     
+     
+     
+     
+     });
