a872e10944fff8e5fd3d340ea5336aa675c0d99e
sridevi peddi <sridevip9@yahoo.com>
2017-01-14 10:31:23 -0600

    all format changes

--- Assessment2/scripts/main.js
+++ Assessment2/scripts/main.js
@@ -3,63 +3,66 @@ document.getElementById('btnSubmit').addEventListener('click', submit);
 //btn.addEventListener('click', submit);
 
 var btn2 = getE("btnreset");
-btn2.addEventListener("click",reset);
+btn2.addEventListener("click", reset);
 
-var inputs = ["firstName","lastName","emailAddress","About"];
+var inputs = ["firstName", "lastName", "emailAddress", "About"];
 var Error_class = "invalid";
-function submit(){
-  var elm;
-  var errorList =[];
 
-  for (var i=0;i < inputs.length;i++){
-      elm = getE(inputs[i]);
-//      console.log(elm);
-    if( elm.value === ""){
-        errorList.push(elm.placeholder + ' is required');
-        elm.classList.add(Error_class);
-    }else{
-         elm.classList.remove(Error_class);
+function submit() {
+  var elm;
+  var errorList = [];
+
+  for (var i = 0; i < inputs.length; i++) {
+    elm = getE(inputs[i]);
+    //      console.log(elm);
+    if (elm.value === "") {
+      errorList.push(elm.placeholder + ' is required');
+      elm.classList.add(Error_class);
+    } else {
+      elm.classList.remove(Error_class);
     }
   }
   var email = getE('emailAddress');
-    if( email.value.indexOf("@") === -1 && email.value.indexOf(".com") === -1){
-       errorList.push('Enter a valid email');
-    }
+  if (email.value.indexOf("@") === -1 && email.value.indexOf(".com") === -1) {
+    errorList.push('Enter a valid email');
+  }
 
-    var hear = getE('hear');
-    if( hear.value === ''){
-      hear.classList.add(Error_class);
-      errorList.push('How did you find us is rquired');
-    }else{
-      hear.classList.remove(Error_class);
-    }
-    var terms = getE('terms');
-    if(terms.checked === false){
-      errorList.push('You must accept terms and conditions')
-    }
-     var html;
-    if(errorList.length > 0){
-      html = " <ul class='errors'><li>" + errorList.join("</li><li>") + " </li></ul>"
-    }else{
-      html = "<h1> Thank you for registering form</h1>"
-      reset();
-    }
-    getE('messages').innerHTML = html;
+  var hear = getE('hear');
+  if (hear.value === '') {
+    hear.classList.add(Error_class);
+    errorList.push('How did you find us is rquired');
+  } else {
+    hear.classList.remove(Error_class);
+  }
+  var terms = getE('terms');
+  if (terms.checked === false) {
+    errorList.push('You must accept terms and conditions')
+  }
+  var html;
+  if (errorList.length > 0) {
+    html = " <ul class='errors'><li>" + errorList.join("</li><li>") + " </li></ul>"
+  } else {
+    html = "<h1> Thank you for registering form</h1>"
+    reset();
+  }
+  getE('messages').innerHTML = html;
 
 }
-function reset(){
-    for(var i = 0;i < inputs.length;i++){
-      var elm = getE(inputs[i]);
-      elm.value = "";
-      elm.classList.remove(Error_class);
-    }
+
+function reset() {
+  for (var i = 0; i < inputs.length; i++) {
+    var elm = getE(inputs[i]);
+    elm.value = "";
+    elm.classList.remove(Error_class);
+  }
 
 
-   getE('messages').innerHTML = "";
-  
-   getE('terms').checked = false;
+  getE('messages').innerHTML = "";
 
-     }
-function getE(id){
-    return document.getElementById(id);
+  getE('terms').checked = false;
+
+}
+
+function getE(id) {
+  return document.getElementById(id);
 }
